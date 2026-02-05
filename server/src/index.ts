8f6368bd37935f3329413875ccbf21dd6270b5e2
Hank Xu <53340720+hankxu1212@users.noreply.github.com>
2026-02-04 17:05:14 -0800

    add ecosystem config

--- server/src/index.ts
+++ server/src/index.ts
@@ -31,7 +31,7 @@ app.use("/users", userRoutes);
 app.use("/teams", teamRoutes);
 app.use("/search", searchRoutes);
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const port = Number(process.env.PORT) || 3000;
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on port ${port}`);
 });
