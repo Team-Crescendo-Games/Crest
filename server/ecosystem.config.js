fb4473248535a1eebde6fc8a529089dd99785c55
Hank Xu <53340720+hankxu1212@users.noreply.github.com>
2026-02-08 01:37:06 -0800

    updated pm2 config

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
         {
-            name: "project-management",
+            name: "crest",
             script: "npm",
             args: "run dev",
             env: {
