3ff6cf9d343cfd78d69b9551ceede7d402ddd85f
Hank Xu <53340720+hankxu1212@users.noreply.github.com>
2026-02-08 02:04:10 -0800

    add more ecosystem config

--- server/ecosystem.config.cjs
+++ server/ecosystem.config.cjs
@@ -1,9 +1,22 @@
-module.exports = { apps : [{ 
-    name: 'crest', 
-    script: 'npm',
-    args: 'run dev', 
-    env: { 
-        NODE_ENV: 'development', 
-        ENV_VAR1: 'environment-variable', 
-    } }], 
+module.exports = {
+  apps: [
+    {
+      name: 'crest-dev',
+      script: 'npm',
+      args: 'run dev',
+      env: {
+        NODE_ENV: 'development',
+      },
+    },
+    {
+      name: 'crest-prod',
+      script: 'tsx',
+      args: 'src/index.ts',
+      instances: 'max',
+      exec_mode: 'cluster',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
 };
